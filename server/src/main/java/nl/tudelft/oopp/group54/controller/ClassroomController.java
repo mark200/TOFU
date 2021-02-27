023bedd348b79b758caf3ce087b1f22d5c98c4df
Bartu Bora Bali <b.b.bali@student.tudelft.nl>
2021-02-27 17:00:36 +0200

    Created API logic module, from which to build on.

---
+++ server/src/main/java/nl/tudelft/oopp/group54/controller/ClassroomController.java
@@ -0,0 +1,21 @@
+package nl.tudelft.oopp.group54.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClassroomController {
+
+
+  private IClassroomLogic logicModule;
+
+//  @GetMapping("/lecture/:lectureId/questions")
+//  @ResponseBody
+//  public Question getAllQuestions() {
+//
+//    return logicModule.getAllQuestions();
+//
+//  }
+
+
+
+}
---
+++ server/src/main/java/nl/tudelft/oopp/group54/entities/Question.java
@@ -0,0 +1,10 @@
+package nl.tudelft.oopp.group54.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "quotes")
+public class Question {
+
+}
