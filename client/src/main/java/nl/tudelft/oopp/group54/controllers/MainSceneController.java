cbf146b795ba490391635b72504cd5f4dc26c787
Bartu Bora Bali <b.b.bali@student.tudelft.nl>
2021-03-02 13:48:32 +0200

    Forgot to add untracked files.

--- client/src/main/java/nl/tudelft/oopp/group54/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/group54/controllers/MainSceneController.java
@@ -1,15 +1,10 @@
 package nl.tudelft.oopp.group54.controllers;
 
-import java.io.IOException;
-
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
+import nl.tudelft.oopp.group54.controllers.AbstractApplicationController;
 import nl.tudelft.oopp.group54.views.ApplicationScene;
 import nl.tudelft.oopp.group54.views.MainView;
 
