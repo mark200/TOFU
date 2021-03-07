60cf93211f48e3cbaa874903945e3a7889afe5c7
Milan de Koning <M.deKoning-1@student.tudelft.nl>
2021-03-07 18:13:03 +0100

    fixed resizing problems

--- client/src/main/java/nl/tudelft/oopp/group54/controllers/LectureRoomSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/group54/controllers/LectureRoomSceneController.java
@@ -2,6 +2,7 @@ package nl.tudelft.oopp.group54.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -19,14 +20,14 @@ public class LectureRoomSceneController extends AbstractApplicationController {
 
   @Override
   public void performControllerSpecificSetup() {
-    for(int i  = 0; i < 3; i++) {
+	questionBox.setFillWidth(true);
+    for(int i  = 0; i < 20; i++) {
       addQuestion("hello world " + i);
     }
   }
 
   public void addQuestion(String question){
     Question q = new Question(question);
-    
     questionBox.getChildren().add(q);
   }
 }
