e7cec28600f8da259313bb47c5311addc6ed7e2e
Apoorva Abhishek <A.Abhishek@student.tudelft.nl>
2021-03-18 15:23:12 +0100

    Improved the QuestionView

--- client/src/main/java/nl/tudelft/oopp/group54/widgets/QuestionView.java
+++ client/src/main/java/nl/tudelft/oopp/group54/widgets/QuestionView.java
@@ -1,5 +1,7 @@
 package nl.tudelft.oopp.group54.widgets;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import nl.tudelft.oopp.group54.models.QuestionModel;
 
 import javafx.beans.property.DoubleProperty;
@@ -22,44 +24,109 @@ public class QuestionView extends AnchorPane {
 
 	VBox innerVBox;
 	GridPane outerGridPane;
-	GridPane innerGridPane;
+	GridPane voteGridPane;
+	GridPane verticalGridPane;
+	GridPane horizontalGridPane;
 	MenuBar menuBar;
 
 	TextArea questionTextArea;
+	Text userName;
+	Button delete;
 
 
 	Button upvoteButton;
-	Button downvoteButton;
 
 	Label currentScore;
-	
+
 	String questionId;
 
 	public QuestionView(String s, String questionId) {
-		AnchorPane anchorPane0 = new AnchorPane();
-		anchorPane0.setMinHeight(Long.MIN_VALUE);
-		anchorPane0.setPrefHeight(400.0);
-		anchorPane0.setMaxHeight(Long.MIN_VALUE);
-		anchorPane0.setPrefWidth(600.0);
-		anchorPane0.setMinWidth(Long.MIN_VALUE);
-		anchorPane0.setMaxWidth(Long.MIN_VALUE);
-		Text text1 = new Text();
-		text1.setStrokeWidth(0.0);
-		//text1.setStrokeType(OUTSIDE);
-		text1.setLayoutX(139.0);
-		text1.setLayoutY(43.0);
-		text1.setText(s);
+		this.outerGridPane = new GridPane();
+		this.voteGridPane = new GridPane();
+		this.verticalGridPane = new GridPane();
+		this.horizontalGridPane = new GridPane();
+		this.innerVBox = new VBox();
+		this.menuBar = new MenuBar();
+		this.delete = new Button("delete");
+		this.questionTextArea = new TextArea(s);
+		this.userName = new Text("bob");
+		this.questionId = questionId;
 
-		// Adding child to parent
-		anchorPane0.getChildren().add(text1);
-		TextArea textArea2 = new TextArea();
-		textArea2.setPrefHeight(200.0);
-		textArea2.setPrefWidth(200.0);
-		textArea2.setLayoutX(246.0);
-		textArea2.setLayoutY(173.0);
+		this.questionTextArea.setWrapText(true);
+		this.questionTextArea.setEditable(false);
+		this.questionTextArea.setPrefRowCount(3);
 
-		// Adding child to parent
-		anchorPane0.getChildren().add(textArea2);
+
+		this.upvoteButton = new Button("^");
+		this.currentScore = new Label("0");
+
+		this.voteGridPane.add(this.upvoteButton, 0, 0);
+		this.voteGridPane.add(this.currentScore, 0, 1);
+
+		GridPane.setHgrow(this.questionTextArea, Priority.ALWAYS);
+
+		this.questionTextArea.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+
+		this.outerGridPane.add(this.voteGridPane, 0, 0);
+		this.outerGridPane.add(this.verticalGridPane, 1, 0);
+
+//		this.innerVBox.fillWidthProperty();
+//
+		ColumnConstraints col1 = new ColumnConstraints();
+		col1.setMinWidth(30);
+
+		ColumnConstraints col2 = new ColumnConstraints();
+		col2.setFillWidth(true);
+		col2.setHgrow(Priority.ALWAYS);
+
+		this.outerGridPane.getColumnConstraints().addAll(col1, col2);
+
+		this.outerGridPane.setStyle("-fx-border-color: red;" + "-fx-border-radius: 3px");
+
+		//outerGridPane.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+
+		this.verticalGridPane.add(this.horizontalGridPane, 0, 0);
+		this.verticalGridPane.add(this.questionTextArea, 0, 1);
+
+
+
+		this.horizontalGridPane.add(this.userName, 0, 0);
+		this.horizontalGridPane.add(this.delete, 1, 0);
+
+
+		this.horizontalGridPane.setStyle("-fx-border-color: aqua;" + "-fx-border-radius: 3px");
+
+
+		ColumnConstraints column1 = new ColumnConstraints();
+		column1.setPercentWidth(50);
+
+		ColumnConstraints column2 = new ColumnConstraints();
+		column2.setHalignment(HPos.RIGHT);
+		column2.setFillWidth(true);
+		column2.setPercentWidth(50);
+
+		this.horizontalGridPane.getColumnConstraints().addAll(column1, column2);
+		//this.delete.setAlignment(Pos.CENTER_RIGHT);
+
+
+
+
+
+		/**
+		 ColumnConstraints cc = new ColumnConstraints();
+		 cc.setPercentWidth(10);
+		 ColumnConstraints cc2 = new ColumnConstraints();
+		 cc.setPercentWidth(90);
+
+		 this.outerGridPane.getColumnConstraints().addAll(cc2, cc);
+		 **/
+
+		upvoteButton.setOnAction(event -> {
+			voteButtonPressed(true);
+		});
+
+
+		this.getChildren().addAll(this.outerGridPane, this.menuBar);
 	}
 
 	private void childConfiguration() {
@@ -71,9 +138,9 @@ public class QuestionView extends AnchorPane {
 		setLeftAnchor(menuBar, 0.0);
 		setRightAnchor(menuBar, 0.0);
 	}
-	
+
 	private void voteButtonPressed(boolean upvote) {
-		
+
 	}
 
 //	public void setQuestionModel() {
