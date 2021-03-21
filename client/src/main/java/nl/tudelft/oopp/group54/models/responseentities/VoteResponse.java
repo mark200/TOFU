2847810af2fd765485a939d3ee68668ee01e497c
Apoorva Abhishek <A.Abhishek@student.tudelft.nl>
2021-03-21 22:45:02 +0100

    feature completed with one bug to fix

--- client/src/main/java/nl/tudelft/oopp/group54/models/responseentities/VoteResponse.java
+++ client/src/main/java/nl/tudelft/oopp/group54/models/responseentities/VoteResponse.java
@@ -6,6 +6,9 @@ public class VoteResponse implements Serializable {
     private boolean success;
     private String message;
 
+    public VoteResponse() {
+    }
+
     public VoteResponse(boolean success, String message) {
         this.success = success;
         this.message = message;
