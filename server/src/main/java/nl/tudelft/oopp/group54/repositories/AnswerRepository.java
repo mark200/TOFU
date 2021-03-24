c0b4dee1076db66e8039ea519cfafcfc9c89a71a
Mark Dragnev <m.n.dragnev@student.tudelft.nl>
2021-03-24 20:39:52 +0200

    Fixed checkstyle of AnswerRepository.java

--- server/src/main/java/nl/tudelft/oopp/group54/repositories/AnswerRepository.java
+++ server/src/main/java/nl/tudelft/oopp/group54/repositories/AnswerRepository.java
@@ -1,10 +1,9 @@
 package nl.tudelft.oopp.group54.repositories;
 
+import java.util.List;
 import nl.tudelft.oopp.group54.entities.Answer;
 import nl.tudelft.oopp.group54.entities.AnswerKey;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, AnswerKey> {
 }
