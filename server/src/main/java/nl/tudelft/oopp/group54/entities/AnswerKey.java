b15ba2438b2352b5bfadc426cdcb2bbe12d4760f
Mark Dragnev <m.n.dragnev@student.tudelft.nl>
2021-03-24 20:38:42 +0200

    Fixed checkstyle of AnswerKey.java

--- server/src/main/java/nl/tudelft/oopp/group54/entities/AnswerKey.java
+++ server/src/main/java/nl/tudelft/oopp/group54/entities/AnswerKey.java
@@ -1,43 +1,45 @@
 package nl.tudelft.oopp.group54.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 public class AnswerKey implements Serializable, Comparable {
-	
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", columnDefinition = "INT AUTO_INCREMENT")
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "INT AUTO_INCREMENT")
     private Integer id;
 
     @Column(name = "lecture_id", nullable = false)
-    private Integer lecture_id;
+    private Integer lectureID;
 
-    public AnswerKey(Integer id, int lecture_id) {
+    public AnswerKey(Integer id, int lectureID) {
         this.id = id;
-        this.lecture_id = lecture_id;
+        this.lectureID = lectureID;
     }
 
-    public AnswerKey(){}
+    public AnswerKey() {
+
+    }
 
     public int getId() {
         return id;
     }
 
     public int getLecture_id() {
-        return lecture_id;
+        return lectureID;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setLecture_id(int lecture_id) {
-        this.lecture_id = lecture_id;
+    public void setLecture_id(int lectureID) {
+        this.lectureID = lectureID;
     }
 
     @Override
@@ -48,11 +50,15 @@ public class AnswerKey implements Serializable, Comparable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         AnswerKey answerKey = (AnswerKey) o;
-        return getId() == answerKey.getId() &&
-                getLecture_id() == answerKey.getLecture_id();
+        return getId() == answerKey.getId()
+                && getLecture_id() == answerKey.getLecture_id();
     }
 
     @Override
