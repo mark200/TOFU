9fc50695d3d3dffd89c9423552a02f744e3af6cd
Mark Dragnev <m.n.dragnev@student.tudelft.nl>
2021-03-25 00:53:54 +0200

    Fixed UserTest.java checkstyle

--- server/src/test/java/nl/tudelft/oopp/demo/entities/UserTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/UserTest.java
@@ -1,5 +1,11 @@
 package nl.tudelft.oopp.demo.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Date;
+
 import nl.tudelft.oopp.group54.entities.User;
 import nl.tudelft.oopp.group54.entities.UserKey;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,75 +13,71 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class UserTest {
     private UserKey key = new UserKey(2, 3);
     private Date date = new Date();
-    private User u = new User(key, "Ivan Ivanov", "127.0.0.1", date, 1);
+    private User user = new User(key, "Ivan Ivanov", "127.0.0.1", date, 1);
 
     @Test
     public void testConstructor() {
-        assertNotNull(u);
+        assertNotNull(user);
     }
 
     @Test
     public void getKeyTest() {
-        assertEquals(new UserKey(2, 3), u.getKey());
+        assertEquals(new UserKey(2, 3), user.getKey());
     }
 
     @Test
     public void setKeyTest() {
         UserKey newKey = new UserKey(3, 4);
-        u.setKey(newKey);
-        assertEquals(newKey, u.getKey());
+        user.setKey(newKey);
+        assertEquals(newKey, user.getKey());
     }
 
     @Test
     public void testGetterName() {
-        assertEquals("Ivan Ivanov", u.getName());
+        assertEquals("Ivan Ivanov", user.getName());
     }
 
     @Test
     public void testSetterName() {
-        u.setName("Pencho Penchev");
-        assertEquals("Pencho Penchev", u.getName());
+        user.setName("Pencho Penchev");
+        assertEquals("Pencho Penchev", user.getName());
     }
 
     @Test
     public void testGetterIpAddress() {
-        assertEquals("127.0.0.1", u.getIpAddress());
+        assertEquals("127.0.0.1", user.getIpAddress());
     }
 
     @Test
     public void testSetterIpAddress() {
-        u.setIpAddress("255.255.255.8");
-        assertEquals("255.255.255.8", u.getIpAddress());
+        user.setIpAddress("255.255.255.8");
+        assertEquals("255.255.255.8", user.getIpAddress());
     }
 
     @Test
     public void testGetterRoleId() {
-        assertEquals(1, u.getRoleID());
+        assertEquals(1, user.getRoleID());
     }
 
     @Test
     public void testSetterRoleId() {
-        u.setRoleID(2);
-        assertEquals(2, u.getRoleID());
+        user.setRoleID(2);
+        assertEquals(2, user.getRoleID());
     }
 
     @Test
     public void testEquals() {
         User u1 = new User(key, "Ivan Ivanov", "127.0.0.1", date, 1);
-        assertEquals(u1, u);
+        assertEquals(u1, user);
     }
 
     @Test
     public void testNotEquals() {
         User u1 = new User(new UserKey(3, 3), "Ivan Ivanov", "127.0.0.1", date, 1);
-        assertNotEquals(u1, u);
+        assertNotEquals(u1, user);
     }
 }
 
