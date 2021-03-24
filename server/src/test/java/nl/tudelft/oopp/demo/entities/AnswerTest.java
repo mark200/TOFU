a2d50f4db6ced51e2461bf3dd06e6c7ebe4ead9c
Apoorva Abhishek <A.Abhishek@student.tudelft.nl>
2021-03-24 23:53:40 +0100

    pipeline hopefully fixed

--- server/src/test/java/nl/tudelft/oopp/demo/entities/AnswerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/AnswerTest.java
@@ -1,12 +1,13 @@
 package nl.tudelft.oopp.demo.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import nl.tudelft.oopp.group54.entities.Answer;
 import nl.tudelft.oopp.group54.entities.AnswerKey;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 public class AnswerTest {
@@ -16,8 +17,11 @@ public class AnswerTest {
     static AnswerKey key;
     static AnswerKey keyDuplicate;
 
+    /**
+     * Init.
+     */
     @BeforeAll
-    public static void init(){
+    public static void init() {
         key = new AnswerKey(123, 321);
         keyDuplicate = new AnswerKey(123, 321);
         answer = new Answer(key, "answer", 1234, 4321);
@@ -26,7 +30,7 @@ public class AnswerTest {
     }
 
     @Test
-    public void equalsPrimaryKeys(){
+    public void equalsPrimaryKeys() {
         assertEquals(answer, answerDuplicate);
     }
 
