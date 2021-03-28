cc1597876bc2a4b6783b7dd09faff461853f14a1
Martynas Krupskis <m.krupskis@student.tudelft.nl>
2021-03-28 13:35:33 +0300

    fixed checkstyle

--- server/src/test/java/nl/tudelft/oopp/group54/services/LectureTest.java
+++ server/src/test/java/nl/tudelft/oopp/group54/services/LectureTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.group54.services;
 
+import java.util.Date;
 import nl.tudelft.oopp.group54.entities.Lecture;
 import nl.tudelft.oopp.group54.repositories.LectureRepository;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Date;
 
 @DataJpaTest
 public class LectureTest {
