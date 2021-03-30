4515ddb3de0df9e50b92e0a7e78419b3fbc81da8
Joosep den Boer <j.denboer-3@student.tudelft.nl>
2021-03-30 15:24:24 +0200

    little checkstyle fix

--- server/src/test/java/nl/tudelft/oopp/group54/entities/LectureFeedbackTest.java
+++ server/src/test/java/nl/tudelft/oopp/group54/entities/LectureFeedbackTest.java
@@ -1,11 +1,12 @@
 package nl.tudelft.oopp.group54.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @DataJpaTest
 public class LectureFeedbackTest {
