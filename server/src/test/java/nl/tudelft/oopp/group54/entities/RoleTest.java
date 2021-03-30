ae506bc5eadb433a6b4d3f6bcce9a622ac75fc4c
Mark Dragnev <m.n.dragnev@student.tudelft.nl>
2021-03-30 04:14:40 +0300

    Full coverage on Role entity.

--- server/src/test/java/nl/tudelft/oopp/group54/entities/RoleTest.java
+++ server/src/test/java/nl/tudelft/oopp/group54/entities/RoleTest.java
@@ -2,16 +2,17 @@ package nl.tudelft.oopp.group54.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class RoleTest {
 
-    Role roleLec;
-    Role roleMod;
-    Role roleStud;
+    private Role roleLec;
+    private Role roleMod;
+    private Role roleStud;
 
     @BeforeEach
     void setUp() {
@@ -21,12 +22,24 @@ class RoleTest {
     }
 
     @Test
-    public void testEquals() {
-        assertEquals(roleLec, roleLec);
+    public void testSetID() {
+        roleLec.setId(4);
+        assertEquals(4, roleLec.getId());
     }
 
     @Test
-    public void equalsSameObject() {
+    public void testGetName() {
+        assertEquals("lecturer", roleLec.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        roleLec.setName("new name");
+        assertEquals("new name", roleLec.getName());
+    }
+
+    @Test
+    public void testEqualsSameObject() {
         assertEquals(roleLec, roleLec);
     }
 
@@ -43,5 +56,21 @@ class RoleTest {
     @Test
     public void equalsDifferentObject() {
         assertNotEquals(roleLec, "123");
+    }
+
+    @Test
+    public void testEqualsTwoRolesWithSameAttributes() {
+        assertEquals(roleLec, new Role(1, "lecturer"));
+    }
+
+    @Test
+    public void testRoleHashCode() {
+        assertEquals(-2004703933, roleMod.hashCode());
+    }
+
+    @Test
+    public void testRoleToString() {
+        String expected = "Role{id=3 - name='student'}";
+        assertEquals(expected, roleStud.toString());
     }
 }
