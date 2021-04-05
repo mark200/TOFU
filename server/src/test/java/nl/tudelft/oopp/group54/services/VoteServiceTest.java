cd3b0b7366712037c6288ab04c4ee81891566f8f
Apoorva Abhishek <A.Abhishek@student.tudelft.nl>
2021-04-05 16:11:19 +0200

    now testing the right way

--- server/src/test/java/nl/tudelft/oopp/group54/services/VoteServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/group54/services/VoteServiceTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -56,8 +58,10 @@ public class VoteServiceTest {
     public void initEach() {
         System.out.println(voteRepositoryMock);
         Date date = new Date();
-        lecture1 = new Lecture(1, "OOPP", date, "student-join", "moderator-join", "lecturer-join", true);
-        lecture2 = new Lecture(2, "LA", date, "student-join", "moderator-join", "lecturer-join", true);
+        lecture1 = new Lecture(1, "OOPP", date, "student-join", "moderator-join",
+                "lecturer-join", true);
+        lecture2 = new Lecture(2, "LA", date, "student-join", "moderator-join",
+                "lecturer-join", true);
 
         user1 = new User(new UserKey(1,1), "Bob", "127.0.0.1", new Date(), 1);
         user2 = new User(new UserKey(2,1), "Nik", "127.0.0.1", new Date(), 3);
@@ -66,13 +70,6 @@ public class VoteServiceTest {
                 "hello", 0, false, new Date());
         question2 = new Question(new QuestionKey(2,1), user2.getKey().getId(), user2.getIpAddress(),
                 "hey", 0, false, new Date());
-
-        lectureRepositoryMock.save(lecture1);
-        lectureRepositoryMock.save(lecture2);
-        userRepositoryMock.save(user1);
-        userRepositoryMock.save(user2);
-        questionRepositoryMock.save(question1);
-        questionRepositoryMock.save(question2);
     }
 
     @Test
@@ -81,10 +78,25 @@ public class VoteServiceTest {
 
         status.put("success", true);
 
-        Vote vote = new Vote(new VoteKey(user1.getKey().getId(), 1, question1.getPrimaryKey().getId()),
-                1);
+        when(lectureRepositoryMock.findById(Mockito.anyInt())).thenReturn(Optional.of(lecture1));
+        when(userRepositoryMock.findById(Mockito.any(UserKey.class))).thenReturn(Optional.of(user1));
+        when(questionRepositoryMock.findById(Mockito.any(QuestionKey.class))).thenReturn(Optional.of(question2));
 
-        Map<String, Object> voted = voteService.voteOnQuestion(1, user1.getKey().getId().toString(), 2, true);
+        Map<String, Object> voted = voteService.voteOnQuestion(1, user1.getKey().getId().toString(),
+                question2.getPrimaryKey().getId(), true);
+
+        assertEquals(status, voted);
+    }
+
+    @Test
+    void voteLectureIdNull() {
+        Map<String, Object> status = new TreeMap<>();
+
+        status.put("success", false);
+        status.put("message", "LectureID cannot be null.");
+
+        Map<String, Object> voted = voteService.voteOnQuestion(null, user1.getKey().getId().toString(),
+                question2.getPrimaryKey().getId(), true);
 
         assertEquals(status, voted);
     }
