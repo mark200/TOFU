9d83482db8859837640278503c22725dd9810555
Martynas Krupskis <m.krupskis@student.tudelft.nl>
2021-04-06 12:58:28 +0300

    lecture controller 100%, this completes the api testing

--- server/src/test/java/nl/tudelft/oopp/group54/controllers/LectureTest.java
+++ server/src/test/java/nl/tudelft/oopp/group54/controllers/LectureTest.java
@@ -1,7 +1,18 @@
 package nl.tudelft.oopp.group54.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 import nl.tudelft.oopp.group54.controllers.lectures.LectureController;
 import nl.tudelft.oopp.group54.controllers.lectures.LectureService;
 import nl.tudelft.oopp.group54.controllers.lectures.LectureServiceImpl;
@@ -28,18 +39,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
-import java.util.Date;
-import java.util.Map;
-import java.util.TreeMap;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 //@RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
 //@ContextConfiguration(classes = {LectureController.class})
@@ -55,6 +54,9 @@ public class LectureTest {
     private LectureController lectureController;
 
 
+    /**
+     * Initialize the mockMvc.
+     */
     @BeforeEach
     public void init() {
         MockitoAnnotations.initMocks(this);
@@ -72,14 +74,17 @@ public class LectureTest {
         when(lectureService.createNewLecture(any(Date.class), anyString())).thenReturn(status);
 
         MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders
-        .post("/lectures")
-        .content("{\n" +
-                "    \"startTime\": 1614451405020,\n" +
-                "    \"lectureName\": \"lecture1\"\n" +
+            .post("/lectures")
+            .content("{\n"
+                    +
+                "    \"startTime\": 1614451405020,\n"
+                    +
+                "    \"lectureName\": \"lecture1\"\n"
+                    +
                 "}")
-        .contentType(MediaType.APPLICATION_JSON_VALUE)
-        .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
-        .andReturn().getResponse();
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+            .andReturn().getResponse();
         System.out.println(result);
 
 
@@ -103,9 +108,103 @@ public class LectureTest {
         System.out.println(result);
 
 
-        assertEquals("{\"message\":\"Expected lectureName and startTime to be provided.\",\"success\":\"false\"}", result.getContentAsString());
+        assertEquals("{\"message\":\"Expected lectureName and startTime "
+                +
+                "to be provided.\",\"success\":\"false\"}", result.getContentAsString());
     }
 
+    @Test
+    public void joinOngoing_info() throws Exception {
+
+        Map<String, Object> status = new TreeMap<>();
+        status.put("success", true);
+
+        when(lectureService.joinOngoingLecture(anyInt(),anyString(), anyString())).thenReturn(status);
+
+        MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/lectures/j/1/1")
+                .content("{\"userName\": \"Martynas\"}")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+                .andReturn().getResponse();
+        System.out.println(result);
+
+
+        assertEquals("{\"success\":true}", result.getContentAsString());
+    }
+
+    @Test
+    public void joinOnGoingLecture_noInfo() throws Exception {
+
+        Map<String, Object> status = new TreeMap<>();
+        status.put("success", true);
+
+        //when(lectureService.createNewLecture(any(Date.class), anyString())).thenReturn(status);
+
+        MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/lectures/j/1/1")
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+                .andReturn().getResponse();
+        System.out.println(result);
+
+
+        assertEquals("{\"message\":\"Expected userName to be provided.\",\"success\":\"false\"}", result.getContentAsString());
+    }
+
+    @Test
+    public void postFeedback_info() throws Exception {
+
+        Map<String, Object> status = new TreeMap<>();
+        status.put("success", true);
+
+        when(lectureService.postLectureFeedback(anyInt(),anyString(), anyInt())).thenReturn(status);
+
+        MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/lectures/1/feedback")
+                .content("{\n"
+                        +
+                        "    \"userId\": 1,\n"
+                        +
+                        "    \"lectureFeedbackCode\": 1\n"
+                        +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+                .andReturn().getResponse();
+
+
+        assertEquals("{\"success\":true}", result.getContentAsString());
+    }
+
+    @Test
+    public void postFeedback_noInfo() throws Exception {
+
+        Map<String, Object> status = new TreeMap<>();
+        status.put("success", true);
+
+        //when(lectureService.createNewLecture(any(Date.class), anyString())).thenReturn(status);
+
+        MockHttpServletResponse result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/lectures/1/feedback")
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+                .andReturn().getResponse();
+        System.out.println(result);
+
+
+        assertEquals("{\"message\":\"Expected userId and lectureFeedbackId"
+                +
+                " to be provided.\",\"success\":\"false\"}", result.getContentAsString());
+    }
+
+    /**
+     * convert Stribg to JSON.
+     * @param obj - string
+     * @return
+     */
     public static String asJsonString(final Object obj) {
         try {
             final ObjectMapper mapper = new ObjectMapper();
